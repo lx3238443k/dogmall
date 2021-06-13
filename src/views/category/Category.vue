55af25dd3716b3cbee9bef9cd5a23108b55ed1bd
dogly <lx3238443k@126.com>
2021-06-14 00:10:42 +0800

    tabbar

---
+++ src/views/category/Category.vue
@@ -0,0 +1,14 @@
+<template>
+    <div >
+        <h2>分类</h2>
+  
+    </div>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  export default {
+    name: 'Home'
+  }
+  </script>
+  
---
+++ src/views/profile/Profile.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="home">
+      <h2>个人中心</h2>
+  
+    </div>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  export default {
+    name: 'Home'
+  }
+  </script>
+  
